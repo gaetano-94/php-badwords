616418c4b86e0c4fa2448217c475e2b51374d2af
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-08 16:29:24 +0100

    feat: form con paragrafo e parola censurata

--- form.php
+++ form.php
@@ -6,7 +6,17 @@
   <title>PHP Badwords</title>
 </head>
 <body>
- 
-  
+  <div>
+    <form action="process.php" method="POST">
+       <!-- paragrafo -->
+      <label for="paragraph">Inserisci il paragrafo:</label>
+      <textarea name="paragraph" id="paragraph" cols="30" rows="4"></textarea><br>
+
+      <!-- // parola censurata -->
+      <label for="wordToCensor">Inserisci la parola da censurare:</label>
+      <input type="text" name="wordToCensor" id="wordToCensor">
+      <button>invia</button>
+    </form>
+  </div>
 </body>
 </html>
