083502671546d09fe1330b85e5dd75142950e53a
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-08 17:33:18 +0100

    feat: stampare paragrafo censurato e lunghezza caratteri

--- process.php
+++ process.php
@@ -6,8 +6,12 @@ $wordToCensor = $_GET["wordToCensor"];
 
 //stampare il paragrafo e la sua lunghezza
 echo "Paragrafo: $paragraph <br>";
-echo "Lunghezza del paragrafo: " . strlen($paragraph) . " caratteri";
+echo "Lunghezza del paragrafo: " . strlen($paragraph) . " caratteri <br>";
 
 //sostituire la parola censurata con ***
 $censoredParagraph = str_ireplace($wordToCensor, '***', $paragraph);
+
+//stampare paragrafo censurato e la sua lunghezza
+echo "Paragrafo censurato: $censoredParagraph <br>";
+echo "Lunghezza del paragrafo censurato: " . strlen($censoredParagraph) . " caratteri";
 ?>
